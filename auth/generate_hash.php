5ea2d5124dc7ff920617aab04d2081d11c50fbca
Soham <hackerethical514@gmail.com>
2025-03-24 10:29:28 +0530

    Remove all content from the repository

--- auth/generate_hash.php
+++
@@ -1,6 +0,0 @@
-<?php
-$password = "Admin115#"; // Change this to your desired password
-$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo "Hashed Password: " . $hashedPassword;
-?>
-
